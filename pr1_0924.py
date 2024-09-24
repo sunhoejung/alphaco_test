60265c48cc6b286477f7c30701279165662fbafe
sunhoe <tnsdl1310@gmail.com>
2024-09-24 17:33:39 +0900

    text

--- pr1_0924.py
+++ pr1_0924.py
@@ -1,4 +1,14 @@
-a = "Aython"
-b = "P"
-c = a[1:]
-print(b+c)
+class fourcal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def add(self):
+        result = self.first + self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    
+a = fourcal(2,4)
+print(a.add(), a.sub())
